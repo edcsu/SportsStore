145608808fd5a6bd09be799062b3819ba308b034
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-03-08 22:48:05 +0300

    style: showcase products

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -4,9 +4,12 @@ import { FormsModule } from "@angular/forms";
 import { ModelModule } from "../model/model.module";
 import { StoreComponent } from "./store.component";
 import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from '@angular/material/icon';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatChipsModule } from '@angular/material/chips';
 
 @NgModule({
-    imports: [ModelModule, BrowserModule, FormsModule, MatCardModule],
+    imports: [ModelModule, BrowserModule, FormsModule, MatCardModule, MatIconModule, MatBadgeModule, MatChipsModule],
     declarations: [StoreComponent],
     exports: [StoreComponent],
     bootstrap: [StoreComponent]
