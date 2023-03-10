d3b768399c71b9c321c5c70bc3461dee6189cefa
Ssewannonda Keith Edwin <skeith696@gmail.com>
2023-03-10 19:12:56 +0300

    feat: add store first guard

--- src/app/store/cartDetail.component.ts
+++ src/app/store/cartDetail.component.ts
@@ -1,5 +1,9 @@
 import { Component } from "@angular/core";
+import { Cart } from "../model/cart.model";
+
 @Component({
-    template: "./cartDetail.component.html"
+    templateUrl: "cartDetail.component.html"
 })
-export class CartDetailComponent {}
+export class CartDetailComponent {
+    constructor(public cart: Cart) { }
+}
